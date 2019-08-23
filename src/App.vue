25810fe5aa62e4327abb2dba1aa8244ef27205cf
Tijani Rijal Fadhilah <tijani.rijal@adxasia.co.id>
2019-08-24 01:11:18 +0700

    Design frontend + saas loader installation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="calculator">
+      <div class="calculator__preview">
+        <h2>{{dummyPreview}}</h2>
+      </div>
+      <div class="calculator__result">
+        <h1>=</h1>
+        <h1 class="amount">{{dummyResult}}</h1>
+      </div>
+      <div class="calculator__buttons">
+        <div class="calculator__buttons__row">
+          <calc-button class="calculator__buttons__row__button" label="CLEAR" type="clear"/>
+          <calc-button class="calculator__buttons__row__button" label="x" type="operator"/>
+        </div>
+        <div class="calculator__buttons__row">
+          <calc-button class="calculator__buttons__row__button" label="1" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="2" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="3" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="+" type="operator"/>
+        </div>
+        <div class="calculator__buttons__row">
+          <calc-button class="calculator__buttons__row__button" label="4" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="5" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="6" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="-" type="operator"/>
+        </div>
+        <div class="calculator__buttons__row">
+          <calc-button class="calculator__buttons__row__button" label="7" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="8" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="9" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="%" type="operator"/>
+        </div>
+        <div class="calculator__buttons__row">
+          <calc-button class="calculator__buttons__row__button" label="c" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="0" type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="." type="number"/>
+          <calc-button class="calculator__buttons__row__button" label="=" type="operator"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from '@/components/button'
 
 export default {
-  name: 'app',
+  name: 'calculator-app',
   components: {
-    HelloWorld
+    'calc-button': Button
+  },
+  data() {
+    return {
+      dummyResult: 123,
+      dummyPreview: '100 + 10 + 13',
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+// @import '~@/styles/base.scss';
+@import '~@/styles/calculator.scss';
 </style>
---
+++ src/components/button/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <button :class="{ 'clear': clear, 'operator': operator, 'number': number }">
+    {{label}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'calculator-button',
+  props: {
+    label: {
+      required: true
+    },
+    type: {
+      required: true
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    number() {
+      if (this.type === 'number') {
+        return true
+      }
+
+      return false
+    },
+    operator() {
+      if (this.type === 'operator') {
+        return true
+      }
+
+      return false
+    },
+    clear() {
+      if (this.type === 'clear') {
+        return true
+      }
+
+      return false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
